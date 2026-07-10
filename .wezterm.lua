2f05d22659e251480646af97bbd57229aaab7178
Haozhe Li <152936311+lihaozhe013@users.noreply.github.com>
2026-07-10 11:48:14 +0800

    Create .wezterm.lua with basic settings
    Add initial configuration for wezterm terminal.

---
+++ .wezterm.lua
@@ -0,0 +1,23 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 10
+config.font = wezterm.font_with_fallback {
+  'Source Code Pro',
+  'Noto Sans CJK SC Regular',
+}
+
+config.color_scheme = 'Catppuccin Macchiato'
+
+-- Finally, return the configuration to wezterm:
+return config
